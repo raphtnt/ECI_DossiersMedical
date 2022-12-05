1e51481a436b548598613f1fa8abf2f0d05a714d
raphtnt <raphtnt@raphtnt>
2022-12-05 15:30:41 +0100

    Ajout de la base de donnée

---
+++ src/database/Request.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Request {
+	
+	Connexion conn = new Connexion("system", "oracle", "172.17.0.3", "1521");
+	Connection c = conn.getConnection();
+	
+	public void test() {
+		Statement stmt;
+		try {
+			
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM emp");
+
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+			}
+			c.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
